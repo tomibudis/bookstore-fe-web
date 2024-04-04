67991d14fd8408f3edc8d0cc897ef4c43fb52afe
tomibudis <tomibudis@gmail.com>
2024-04-05 01:27:21 +0700

    fix: auth

--- src/hooks/use-get-profile.ts
+++ src/hooks/use-get-profile.ts
@@ -1,12 +1,20 @@
 import { LOCALSTORAGE_KEY } from "~constants/index";
 import useGetUserById from "./queries/use-get-profile";
+import { useEffect } from "react";
 
 const useGetProfile = () => {
   const userInfo =
     typeof window !== "undefined" &&
     JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-  const response = useGetUserById({ id: userInfo.id });
+  useEffect(() => {
+    if (!userInfo) {
+      window.location.href = "/login";
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const response = useGetUserById({ id: userInfo?.id });
   return response;
 };
 
